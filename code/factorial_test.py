2e2cc9e42077d7a651d30a817f1e5b9a52896c53
Bill <admin@ibm.com>
2018-11-22 09:32:36 +0800

    add some *.py

--- code/factorial_test.py
+++ code/factorial_test.py
@@ -1,5 +1,5 @@
 import unittest
-from factorial import fact
+from factorial import fact, div
 
 
 class TestFactorial(unittest.TestCase):
@@ -7,6 +7,9 @@ class TestFactorial(unittest.TestCase):
 	def test_fact(self):
 		res = fact(5)
 		self.assertEqual(res, 120)
+	
+	def test_error(self):
+		self.assertRaises(ZeroDivisionError, div, 0)
 		
 if __name__ == '__main__':
 	unittest.main()
