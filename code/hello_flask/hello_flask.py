3720c0b975db031cbea31aa53fcf002589c84679
bens0n <bens0n@pro.local>
2018-11-25 17:38:09 +0800

    update flask app

--- code/hello_flask/hello_flask.py
+++ code/hello_flask/hello_flask.py
@@ -2,11 +2,17 @@
 
 import flask
 
+
+# Create the application.
 APP = flask.Flask(__name__)
+
 
 @APP.route('/')
 def index():
-    return flask.render_templates('index.html')
+    """ 显示可在 '/' 访问的 index 页面
+    """
+    return flask.render_template('index.html')
+
 
 if __name__ == '__main__':
     APP.debug=True
